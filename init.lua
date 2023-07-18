4ffc93a59ab27d8f63d3b75d30432ce4b975bf62
João Baptista <jbaptista@liveperson.com>
2023-07-18 11:24:50 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = ','
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+vim.opt.termguicolors = true
+vim.opt.clipboard = 'unnamedplus'
+
+vim.keymap.set('i', 'jk', '<esc>')
+
---
+++ lua/joao/plugins.lua
@@ -0,0 +1,17 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'pangloss/vim-javascript'
+    use 'HerringtonDarkholme/yats.vim'
+    use 'rust-lang/rust.vim'
+    use 'tanvirtin/monokai.nvim'
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', requires = { {'nvim-lua/plenary.nvim'} } }
+end)
