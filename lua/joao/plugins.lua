49e413e6b16a540ba5eec0ff5c25d79322a8942a
João Baptista <jbaptista@liveperson.com>
2023-07-30 07:47:42 +0100

    Add lualine

--- lua/joao/plugins.lua
+++ lua/joao/plugins.lua
@@ -9,13 +9,13 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'Raimondi/delimitMate'
-    use 'pangloss/vim-javascript'
     use 'HerringtonDarkholme/yats.vim'
     use 'rust-lang/rust.vim'
     use { 'dracula/vim', as = 'dracula' }
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', requires = { {'nvim-lua/plenary.nvim'} } }
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
+    use 'nvim-lualine/lualine.nvim'
     use {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
