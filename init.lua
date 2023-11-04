75b04fb15bfba376fad592580b5d8831902584a5
João Baptista <maybejoao@gmail.com>
2023-11-04 09:36:43 +0000

    Use lua/plugins/lazy.lua and simplify after/plugin

--- init.lua
+++ init.lua
@@ -24,5 +24,18 @@ vim.keymap.set('i', 'jk', '<esc>')
 
 vim.g.delimitMate_expand_cr = true
 
-require('joao.plugins')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('plugins')
 
