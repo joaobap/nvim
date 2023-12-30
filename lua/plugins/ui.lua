9f25fa32ac0721b1a6d670fc245ecd8c51f00f49
João Baptista <maybejoao@gmail.com>
2023-12-30 19:35:44 +0000

    Use habamax as theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,10 @@
 return {
-    {
-        'dracula/vim',
-        as = 'dracula',
-        config = function()
---            vim.cmd([[colorscheme dracula]])
-        end
-    },
-    {
-        'craftzdog/solarized-osaka.nvim',
-        config = function()
-            vim.cmd([[colorscheme solarized-osaka-night]])
-        end
-    },
+    -- {
+    --     'craftzdog/solarized-osaka.nvim',
+    --     config = function()
+    --         vim.cmd([[colorscheme solarized-osaka-night]])
+    --     end
+    -- },
     {
         'nvim-lualine/lualine.nvim',
         opts = {
