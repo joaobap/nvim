15c73eb5e91fb8025a42813112d93d9d985ae721
João Baptista <maybejoao@gmail.com>
2024-04-09 10:27:05 +0100

    Add auto-dark mode

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,17 +2,28 @@ return {
     {
         'catppuccin/nvim',
         name = 'catppuccin',
-        config = function()
-            vim.cmd([[colorscheme catppuccin-mocha]])
-        end
+    },
+    {
+        'f-person/auto-dark-mode.nvim',
+        config = {
+            update_interval = 1000,
+            set_dark_mode = function()
+                vim.api.nvim_set_option("background", "dark")
+                vim.cmd("colorscheme catppuccin")
+            end,
+            set_light_mode = function()
+                vim.api.nvim_set_option("background", "light")
+                vim.cmd("colorscheme catppuccin")
+            end,
+        },
     },
     {
         'nvim-lualine/lualine.nvim',
-         opts = {
-             options = {
-                 section_separators = { left = '', right = ''},
-             }
-         }
+        opts = {
+            options = {
+                section_separators = { left = '', right = ''},
+            }
+        }
     },
 }
 
