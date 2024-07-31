b36d7378d73c1d7fe4349a1ce9bdf03a7b5ded68
João Baptista <maybejoao@gmail.com>
2024-07-31 23:39:16 +0100

    Use default colors

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,17 @@
 -- We don't need no syntax
-vim.cmd([[syntax off]])
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        client.server_capabilities.semanticTokensProvider = nil
-    end,
-});
+-- vim.cmd([[syntax off]])
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--     callback = function(args)
+--         local client = vim.lsp.get_client_by_id(args.data.client_id)
+--         client.server_capabilities.semanticTokensProvider = nil
+--     end,
+-- });
 
 return {
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {}
-    },
+--    {
+--        "lukas-reineke/indent-blankline.nvim",
+--        main = "ibl",
+--        opts = {}
+--    },
 }
 
