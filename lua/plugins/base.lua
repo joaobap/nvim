d865372b1ed4c5b70bb250fa9966fae4e8684d72
João Baptista <maybejoao@gmail.com>
2025-01-02 22:46:25 +0000

    Use solarized osaka

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -6,23 +6,17 @@ return {
     { 'echasnovski/mini.pairs', opts = {} },
     { 'lewis6991/gitsigns.nvim', opts = {} },
     {
-        'craftzdog/solarized-osaka.nvim',
-        opts  = {
-            transparency = true
-        }
-    },
-    {
         "f-person/auto-dark-mode.nvim",
-        dependencies = { 'folke/tokyonight.nvim' },
+        dependencies = { 'craftzdog/solarized-osaka.nvim' },
         opts = {
             update_interval = 1000,
             set_dark_mode = function()
                 vim.api.nvim_set_option_value("background", "dark", {})
-                vim.cmd([[colorscheme retrobox]])
+                vim.cmd([[colorscheme solarized-osaka]])
             end,
             set_light_mode = function()
                 vim.api.nvim_set_option_value("background", "light", {})
-                vim.cmd([[colorscheme retrobox]])
+                vim.cmd([[colorscheme solarized-osaka]])
             end,
         },
     },
